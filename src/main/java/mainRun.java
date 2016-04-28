2d40790474df8da5f92908a8cadcc129183a0c67
amirc2013 <Amir.c20132gmail.com>
2016-04-28 14:42:07 +0300

    milestone 1 create table with false args

--- src/main/java/mainRun.java
+++ src/main/java/mainRun.java
@@ -1,4 +1,6 @@
-import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
 /**
  * Created by Amir on 4/27/2016.
@@ -7,6 +9,35 @@ public class mainRun {
 
     public static void main(String[] args){
 
+        connectToDatabase();
+
+
+    }
+
+
+    public static void connectToDatabase() {
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:Store.db");
+            System.out.println("Opened database successfully");
+
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE HOVALOT " +
+                    "(ID INT PRIMARY KEY     NOT NULL," +
+                    " NAME           TEXT    NOT NULL, " +
+                    " AGE            INT     NOT NULL, " +
+                    " ADDRESS        CHAR(50), " +
+                    " SALARY         REAL)";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Table created successfully");
     }
 
 
